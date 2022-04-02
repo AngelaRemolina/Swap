6f1d1b4ebcc07f5161d9fcb5a511e9a3f8d1f347
gabriela lara <laragabriela490@gmail.com>
2022-04-02 11:26:28 -0500

    conexion pc gaby

--- Swap/App.js
+++ Swap/App.js
@@ -1,24 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  console.log("App excecuted!"); // debug 
-  let contPress = 0;
-  let handlePress = () => {
-    contPress += 1;
-    console.log("Text pressed",contPress,"times");
-  }
+  console.log("App execute!");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hola Mundo!!</Text>
-      <Image source={require('./assets/icon.png')}/>
-      <Image source={{
-        uri:"https://picsum.photos/200/300",
-        width: 200,
-        height: 300
-        }}
-      />
+      <Text>Hola amigos! :3</Text>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -27,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
