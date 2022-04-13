9325643855635eaac90753e65d3e1fb263e4ac00
gabriela lara <laragabriela490@gmail.com>
2022-04-13 12:40:03 -0500

    About

--- Swap/App.js
+++ Swap/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Home from "./Screens/Home";
+import ClothingDetail from "./Screens/ClothingDetail";
 
 export default function App() {
 
-  return <Home />;
+  return <ClothingDetail />;
 
 }
--- Swap/components/clothingDetail/About.js
+++ Swap/components/clothingDetail/About.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+const image = "https://http2.mlstatic.com/D_NQ_NP_893128-MCO46091066888_052021-W.jpg"
+const title = 'Camisa de Mickey'
+const description = 'grande'
 
 
 export default function About() {
     return (
         <View>
-            <Text>About</Text>
+            <ClothingImage image={image} />
+            <ClothingTitle title={title} />
+            <ClothingDescription description={description} />
         </View>
     )
 }
+
+const ClothingImage = (props) => (
+    <Image source={{ uri: props.image }} style={{ width: "100", height: 180 }} />
+);
+
+const ClothingTitle = (props) => (
+    <Text style={{
+        fontSize: 29,
+        fontWeight: "600",
+        marginTop: 10,
+        marginHorizontal: 15
+    }}>
+        {props.title}
+    </Text>
+);
+
+const ClothingDescription = (props) => (
+    <Text style={{
+        marginTop: 10,
+        marginHorizontal: 15,
+        fontSize: 15.5,
+        fontWeight: "600",
+    }}>
+        {props.description}
+    </Text>
+);
