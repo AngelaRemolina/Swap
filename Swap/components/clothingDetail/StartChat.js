f224ee197a5c647bb807876dcc85bc85e6e998c7
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2022-04-19 16:52:55 -0500

    Chat screen creation

--- Swap/components/clothingDetail/StartChat.js
+++ Swap/components/clothingDetail/StartChat.js
@@ -1,49 +1,10 @@
-import { StyleSheet, View, Text, Modal, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import ItemSummary from "./ItemSummary";
 
-export default function StartChat(props) {
-  const [modalVisible, setModalVisible] = useState(false);
-  const startChatContent = () => {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 30,
-          backgroundColor: "gray",
-        }}
-      >
-        <ItemSummary item={props} />
-        <View
-          style={{
-            backgroundColor: "black",
-            padding: 10,
-            borderRadius: 30,
-            width: 150,
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={() => setModalVisible(false)}>
-            <Text style={{ color: "white" }}>
-              ¿Estás seguro que quieres iniciar un chat con el vendedor?
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  };
+export default function StartChat({ navigation, ...props }) {
   return (
     <>
-      <Modal
-        animationType="slide"
-        visible={modalVisible}
-        transparent={true}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        {startChatContent()}
-      </Modal>
       <View
         style={{
           alignItems: "center",
@@ -59,7 +20,13 @@ export default function StartChat(props) {
             borderRadius: 30,
             width: 300,
           }}
-          onPress={() => setModalVisible(true)}
+          onPress={() =>
+            navigation.navigate("Chat", {
+              name: props.name,
+              image: props.image,
+              price: props.price,
+            })
+          }
         >
           <Text style={{ color: "white", fontSize: 20 }}>Iniciar Chat</Text>
         </TouchableOpacity>
--- Swap/navigation.js
+++ Swap/navigation.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import Home from './Screens/Home';
-import ClothingDetail from './Screens/ClothingDetail';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import Home from "./Screens/Home";
+import ClothingDetail from "./Screens/ClothingDetail";
+import Chat from "./Screens/Chat";
 
 export default function RootNavigation() {
+  const Stack = createStackNavigator();
 
-    const Stack = createStackNavigator();
+  const screenOptions = {
+    headerShown: false,
+  };
 
-    const screenOptions = {
-        headerShown: false,
-    }
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-                <Stack.Screen name='Home' component={Home} />
-                <Stack.Screen name='ClothingDetail' component={ClothingDetail} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ClothingDetail" component={ClothingDetail} />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
